5f14e4922f0fe322aa298d0dca848fad3f3a1206
wavcita <wavcita@hotmail.com>
2023-03-12 17:59:51 -0400

    lab 3

---
+++ tests/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://devexpress.github.io/testcafe/example/');
+  await page.getByTestId('name-input').click();
+  await page.getByTestId('name-input').fill('Carly');
+  await page.getByTestId('reusing-js-code-checkbox').check();
+  await page.getByTestId('ci-checkbox').check();
+  await page.getByTestId('linux-radio').check();
+  await page.getByTestId('preferred-interface-select').selectOption('JavaScript API');
+  await page.getByTestId('tried-testcafe-checkbox').check();
+  await page.locator('#slider').click();
+  await page.getByTestId('comments-area').click();
+  await page.getByTestId('comments-area').fill('pruebas');
+  //await page.getByTestId('submit-button').click();
+});
